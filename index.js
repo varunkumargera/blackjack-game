f10be9342dffe59f7d87c9d78d185a1fd893186d
varunkumargera <100475526+varunkumargera@users.noreply.github.com>
2022-07-05 18:05:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+let player ={
+	name: "Varun",
+	chips: 200
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = " "
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+console.log(cards)
+function getRandomCard()  {
+	
+	let randomNumber = Math.floor(Math.random()*13)+1
+	if (randomNumber > 10){
+		return 10
+	}else if (randomNumber == 1) {
+		return 11
+	}else {
+		return randomNumber
+	}
+}
+
+
+
+function startGame()   {
+	isAlive = true
+	let firstCard = getRandomCard()
+	let secondCard = getRandomCard()
+	cards= [firstCard +secondCard]
+	sum = firstCard + secondCard
+	renderGame()
+}
+
+function renderGame()  {
+	cardsEl.textContent = "Cards: "
+	for (let i=0 ; i < cards.length; i++){
+		cardsEl.textContent += cards[i] + " "
+	}
+	
+	sumEl.textContent = "Cards: "
+		if(sum <= 20){
+		message="Do you want to draw a neww card ? "
+	} else if (sum === 21){
+		 message=" You've got BlackJack! "
+		 hasBlackJack = true
+	}else  {
+		message="You're out of the game"
+		isAlive =false
+	}
+	messageEl.textContent = message
+	
+}
+
+function newCard() {
+	console.log("Drawing a new card from the deck")
+	if(isAlive === true && hasBlackJack === false) {
+	
+	let card = getRandomCard()
+	sum += card
+	cards.push(card)
+	console.log(cards)
+	renderGame()
+	}
+}
